6478afa3e0d2a7750e6109caa499646a9becbfe6
Talha-MERN <insighttechsolutionz@gmail.com>
2025-02-08 22:33:46 +0500

    minor change

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="w-[100%] flex justify-center text-center items-center h-screen">
         <div className="w-[75%] sm:w-[35%] bg-gray-100 rounded-lg p-5">
           <h1 className="bg-black text-white rounded-lg p-5 text-xl">
-            RTC-MERN
+            Real Time Chat Application
           </h1>
           <MUI_Tabs />
         </div>
